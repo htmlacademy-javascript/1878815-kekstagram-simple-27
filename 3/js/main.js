8d4f2999fc06c0cc37f8520cc819625a267056ef
Keks <keks@htmlacademy.ru>
2022-11-11 14:36:16 +0300

    :heavy_check_mark: Сборка #3

---
+++ 3/js/main.js
@@ -0,0 +1,6 @@
+import './pictures.js';
+import {createPictures} from './pictures.js';
+import {similarListFragment} from './pictures.js';
+
+createPictures();
+similarListFragment();
---
+++ 3/js/pictures.js
@@ -0,0 +1,26 @@
+import './data.js';
+import {getCaptions} from './data.js';
+getCaptions();
+
+const pictures = document.querySelector('.pictures');
+const pictureTemplete = document.querySelector('#picture').content.querySelector('.picture');
+
+const createPictures = getCaptions();
+const similarListFragment = document.createDocumentFragment();
+
+createPictures.forEach(({url, description, comments, likes}) => {
+  const picture = pictureTemplete.cloneNode(true);
+  picture.querySelector('.picture__img').src = url;
+  picture.querySelector('.picture__img').alt = description;
+  picture.querySelector('.picture__comments').textContent = comments;
+  picture.querySelector('.picture__likes').textContent = likes;
+  similarListFragment.append(picture);
+});
+
+pictures.append(similarListFragment);
+
+export {
+  createPictures,
+  similarListFragment
+};
+
